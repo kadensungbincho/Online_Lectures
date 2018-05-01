848b0597062635dc14d7dc1301bdb7e26c114c19
kadensungbincho <kadensungbincho@gmail.com>
2018-05-02 00:18:52 +0900

    learning modules

--- Saenko/javascript/nodejs/nodejs/app.js
+++ Saenko/javascript/nodejs/nodejs/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 app.locals.pretty = true;
 
 // set template engine
@@ -8,13 +9,38 @@ app.set('views', './views');
 
 // static file handling
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false}));
+
 app.get('/topic', function(req, res){
-    res.send(req.query.id+','+ req.query.name);
+    var topics = [
+        'JavaScript is...',
+        'Nodejs is...',
+        'Express is...'
+    ];
+    var output = `
+    <a href="/topic?id=0">JavaScript</a><br>
+    <a href="/topic?id=1">Nodejs</a><br> 
+    <a href="/topic?id=2">Express</a><br><br>
+    ${topics[req.query.id]}
+    `
+    res.send(output);
 });
 
 app.get('/template', function(req, res) {
     res.render('index', {_title:'Jade', time:Date()});
 });
+
+app.get('/form', function(req, res) {
+    res.render('form');
+});
+app.get('/form_receiver', function(req, res) {
+    res.send('Hello GET');
+});
+app.post('/form_receiver', function(req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(title+', '+description);
+})
 
 app.get('/', (req, res) => res.send('Hello Kaden Sungbin Cho!!'));
 // app.get('/route', (req, res) => res.send('Hello Router, <img src="/route.png">'));
