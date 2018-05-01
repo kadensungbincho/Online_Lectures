e1fa05c07759db9c4466364ed72b619ee7152a8f
kadensungbincho <kadensungbincho@gmail.com>
2018-05-01 20:25:25 +0900

    on express and template engine

---
+++ Saenko/javascript/nodejs/nodejs/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const app = express();
+app.locals.pretty = true;
+
+// set template engine
+app.set('view engine', 'jade');
+app.set('views', './views');
+
+// static file handling
+app.use(express.static('public'));
+
+app.get('/template', function(req, res) {
+    res.render('index', {_title:'Jade', time:Date()});
+});
+
+app.get('/', (req, res) => res.send('Hello Kaden Sungbin Cho!!'));
+// app.get('/route', (req, res) => res.send('Hello Router, <img src="/route.png">'));
+
+app.get('/dynamic', function(req, res) {
+    var lis = '';
+    var time = Date();
+
+    for (var i=0; i<5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var output = `
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <title></title>
+        </head>
+        <body>
+            Hello Static!!
+            ${lis}
+            ${time}
+        </body>
+    </html>
+    `
+    res.send(output);
+});
+app.get('/login', (req, res) => res.send('<h1>Log in please</h1>'));
+app.get('/game', (req, res) => res.send('<h2>Gaming!!</h2>'));
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
--- Saenko/javascript/nodejs/nodejs/webserver.js
+++ Saenko/javascript/nodejs/nodejs/webserver.js
@@ -1,11 +1,21 @@
 const http = require('http');
 
-const hostname = '0.0.0.0';
-const port = 6006;
+const hostname = '127.0.0.1';
+const port = 1337;
 
+/*
 http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
     }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
     });
+*/
+
+var server =  http.createServer(function(req, res){
+    res.writeHead(200, {'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+});
+server.listen(port, hostname, function(){
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
