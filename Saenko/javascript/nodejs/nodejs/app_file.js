b833185a4542312f652b46f6a0379e295f4d3617
kadensungbincho <kadensungbincho@gmail.com>
2018-05-02 22:39:04 +0900

    fs

--- Saenko/javascript/nodejs/nodejs/app_file.js
+++ Saenko/javascript/nodejs/nodejs/app_file.js
@@ -2,10 +2,27 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
+var multer = require('multer');
+var storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, 'uploads/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname + '-' + Date.now())
+    }
+})
+var upload = multer({ storage: storage})
 app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
 app.set('views', './views_file');
 app.set('view engine', 'jade');
+app.get('/upload', function(req, res){
+    res.render('upload');
+});
+app.post('/upload', upload.single('userfile'), function(req, res) {
+    console.log(req.file);
+    res.send('Uploaded'+req.file.filename);
+});
 
 app.get('/topic/new', function(req, res) {
     fs.readdir('data', function(err, files){
