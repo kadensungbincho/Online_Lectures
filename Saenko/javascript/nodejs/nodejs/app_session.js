aa60751badbe6b9008b18e45aa186ac8599f8453
kadensungbincho <kadensungbincho@gmail.com>
2018-05-12 15:25:03 +0900

    session with express-session

---
+++ Saenko/javascript/nodejs/nodejs/app_session.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var session = require('express-session');
+var app = express();
+app.use(session({
+    secret: 'aniaoiewr929!@!$#%0jfingraevnwrieocsmlfnawon',
+    resave: false,
+    saveUninitialized: true
+}));
+app.get('/count', function(req, res) {
+    if(req.session.count) {
+        req.session.count++;
+    } else {
+        req.session.count = 1;
+    }
+    res.send('count : ' + req.session.count);
+});
+app.get('/tmp', function(req,res) {
+    res.send('result : ' + req.session.count);
+});
+app.listen(3003, function(){
+    console.log('Connected 3003 port!!!');
+});
