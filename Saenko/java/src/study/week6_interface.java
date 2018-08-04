d9e3bfc0389383da6568e82dbdc37caf7c770936
kadensungbincho <kadensungbincho@gmail.com>
2018-08-04 11:08:04 +0900

    week6 test

---
+++ Saenko/java/src/study/week6_interface.java
@@ -0,0 +1,18 @@
+package study;
+
+interface I {
+	public void z();
+}
+
+class G implements I {
+	public void z() {};
+}
+
+public class week6_interface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
