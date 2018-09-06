1f669a1fb0e6cc1acef8cc325ec509f23a72cb2c
kadensungbincho <kadensungbincho@gmail.com>
2018-09-07 00:26:54 +0900

    update django projects

--- Udemy/PythonEcommerce/ecommerce/src/ecommerce/views.py
+++ Udemy/PythonEcommerce/ecommerce/src/ecommerce/views.py
@@ -1,14 +1,16 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, get_user_model
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from .forms import ContactForm, LoginForm
+from .forms import ContactForm, LoginForm, RegisterForm
 
 def home_page(request):
     context = {
         "title":"Hello World!",
         "content":"Welcome to the homepage"
     }
+    if request.user.is_authenticated():
+        context["premium_content"] = "YEAHHHH"
     return render(request, "home_page.html", context)
 
 
@@ -61,12 +63,20 @@ def login_page(request):
             print("Error")
     return render(request, "auth/login.html", context)
 
-
+User = get_user_model()
 def register_page(request):
-    form = LoginForm(request.POST or None)
+    form = RegisterForm(request.POST or None)
+    context = {
+        "form" : form
+    }
     if form.is_valid():
         print(form.cleaned_data)
-    return render(request, "auth/register.html", {})
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        print(new_user)
+    return render(request, "auth/register.html", context)
 
 
 def home_page_old(request):
