21ed0fb8c219dc69afb71ae525ef8f0f9602a4d9
kadensungbincho <kadensungbincho@gmail.com>
2018-09-08 00:30:20 +0900

    update' ]

--- Udemy/PythonEcommerce/ecommerce/src/ecommerce/urls.py
+++ Udemy/PythonEcommerce/ecommerce/src/ecommerce/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.conf.urls import url
 from django.contrib import admin
 
@@ -26,3 +29,7 @@ urlpatterns = [
     url(r'^register/$', register_page),
     url(r'^admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
