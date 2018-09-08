66cd3056fa892de50a53b4afefbb8907c8b7d076
kadensungbincho <kadensungbincho@gmail.com>
2018-09-08 20:22:16 +0900

    update

--- Udemy/PythonEcommerce/ecommerce/src/ecommerce/urls.py
+++ Udemy/PythonEcommerce/ecommerce/src/ecommerce/urls.py
@@ -19,6 +19,8 @@ from django.conf.urls.static import static
 from django.conf.urls import url
 from django.contrib import admin
 
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -27,6 +29,10 @@ urlpatterns = [
     url(r'^contact/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^register/$', register_page),
+    url(r'^products/$', ProductListView.as_view()),
+    url(r'^products-fbv/$', product_list_view),
+    url(r'^products/(?P<pk>\d*)$', ProductDetailView.as_view()),
+    url(r'^products-fbv/(?P<pk>\d*)$', product_detail_view),
     url(r'^admin/', admin.site.urls),
 ]
 
---
+++ Udemy/PythonEcommerce/ecommerce/src/products/views.py
@@ -0,0 +1,77 @@
+from django.http import Http404
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
+
+from .models import Product
+# Create your views here.
+
+class ProductListView(ListView):
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+    
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
+    def get_queryset(self, *args, **kwargs):
+        request = self.request
+        return Product.objects.all()
+
+
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, "products/list.html", context)
+
+
+class ProductDetailView(DetailView):
+    # queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+    def get_object(self, *args, **kwargs):
+        request = self.request
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Product does not exist")
+        return instance
+
+    # def get_queryset(self, *args, **kwargs):
+    #     request = self.request
+    #     pk = self.kwargs.get('pk')
+    #     return Product.objects.filter(pk=pk)
+
+
+def product_detail_view(request, pk=None, *args, **kwargs):
+    # instance = Product.objects.get(pk=pk)
+    # instance = get_object_or_404(Product, pk=pk)
+    # try:
+    #     instance = Product.objects.get(id=pk)
+    # except Product.DoesNotExist:
+    #     print('no product here')
+    #     raise Http404("Product doesnt exist")
+    # except:
+    #     print('Huh?')
+
+    instance = Product.objects.get_by_id(pk)
+    if instance is None:
+        raise Http404("Product does not exist")
+    # qs = Product.objects.filter(id=pk)
+    # print(qs)
+    # if qs.exists() and qs.count() == 1: # len(qs)
+    #     instance = qs.first()
+    # else:
+    #     raise Http404("Product does not exist")
+
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
