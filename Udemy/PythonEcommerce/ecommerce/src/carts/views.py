5ee2ba81bceac868bb5f2aeabd26ff005cb5d819
kadensungbincho <kadensungbincho@gmail.com>
2018-09-19 09:19:46 +0900

    checkout of billing_profile

--- Udemy/PythonEcommerce/ecommerce/src/carts/views.py
+++ Udemy/PythonEcommerce/ecommerce/src/carts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 
 from accounts.forms import LoginForm, GuestForm
+from accounts.models import GuestEmail
 from billing.models import BillingProfile
 from orders.models import Order
 from products.models import Product
@@ -36,20 +37,35 @@ def checkout_home(request):
     order_obj = None
     if cart_created or cart_obj.products.count() == 0:
         return redirect("cart:home")
-    else:
-        order_obj, new_order_obj = Order.objects.get_or_create(cart=cart_obj)
     user =  request.user
     login_form = LoginForm()
     guest_form = GuestForm()
-
+    guest_email_id = request.session.get('guest_email_id')
     if user.is_authenticated():
         # if user.email:
         billing_profile, billing_profile_created = BillingProfile.objects.get_or_create(
                                                         user=user, email=user.email)
+    elif guest_email_id is not None:
+        guest_email_obj = GuestEmail.objects.get(id=guest_email_id)
+        billing_profile, billing_guest_profile_created = BillingProfile.objects.get_or_create(
+                                                            email=guest_email_obj.email)
+    else:
+        billing_profile = None
+
+    if billing_profile is not None:
+        order_qs = Order.objects.filter(billing_profile=billing_profile, cart=cart_obj)
+        if order_qs.count() == 1:
+            order_obj = order_qs.first()
+        else:
+            old_order_qs = Order.objects.exclude(billing_profile=billing_profile, cart=cart_obj).filter(cart=cart_obj, active=True)
+            if old_order_qs.exists():
+                old_order_qs.update(active=False)
+            order_obj = Order.objects.create(billing_profile=billing_profile, cart=cart_obj)
 
     context = {
         "object": order_obj,
         "billing_profile": billing_profile,
-        "login_form": login_form
+        "login_form": login_form,
+        "guest_form": guest_form
     }
     return render(request, "carts/checkout.html", context)
