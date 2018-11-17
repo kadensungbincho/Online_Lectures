da02c4d4a5e4b936d69669a02ae806f27bc51e23
kadensungbincho <kadensungbincho@gmail.com>
2018-11-17 14:02:57 +0900

    further on meteor

--- Udemy/fullstack_web_apps_with_meteor_and_react/meteor-projects/score-keep/client/main.js
+++ Udemy/fullstack_web_apps_with_meteor_and_react/meteor-projects/score-keep/client/main.js
@@ -5,41 +5,49 @@ import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
 
-Tracker.autorun(function (){
-  console.log('Players list', Players.find().fetch());
-});
 
-const players = [{
-  _id: '1',
-  name: 'Lauroen',
-  score: 99
-}, {
-  _id: '2',
-  name: 'Cory',
-  score: -1
-}, {
-  _id: '3',
-  name: 'Kaden',
-  score: -12
-}];
-
-const renderPlayers = function (players) {
-  return players.map(function (player) {
-    return <p key={player._id}>{player.name} has {player.score} point(s).</p>;
-  });
+const renderPlayers = (players) => {
+  return players.map((player) => (
+    <p key={player._id}>
+      {player.name} has {player.score} point(s).
+      <button onClick={() => Players.update({_id: player._id}, {$inc: {score: -1}})}>-1</button>
+      <button onClick={() => Players.update({_id: player._id}, {$inc: {score: 1}})}>+1</button>
+      <button onClick={() => Players.remove({_id: player._id})}>X</button>
+    </p>
+  ));
 };
 
-Meteor.startup(function () {
-  let title = 'Score Keep';
-  let name = 'Kaden Sungbin Cho';
-  let jsx = (
-    // title -> Account Settings
-    <div>
-      <h1>{title}</h1>
-      <p>Hello {name}!</p>
-      <p>This is my second p.</p>
-      {renderPlayers(players)}
-    </div>
-  );
-  ReactDOM.render(jsx, document.getElementById('app'));
+const handleSubmit = function (e) {
+  let playerName = e.target.playerName.value;
+
+  e.preventDefault();
+
+  if (playerName) {
+    e.target.playerName.value = '';
+    Players.insert({
+      name: playerName,
+      score: 0
+    })
+  }
+};
+
+Meteor.startup(() => {
+  Tracker.autorun(() => {
+    let players = Players.find().fetch();
+    let title = 'Score Keep';
+    let name = 'Kaden Sungbin Cho';
+    let jsx = (
+      <div>
+        <h1>{title}</h1>
+        <p>Hello {name}!</p>
+        <p>This is my second p.</p>
+        {renderPlayers(players)}
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="playerName" placeholder="Player name"/>
+          <button>Add player</button>
+        </form>
+      </div>
+    );
+    ReactDOM.render(jsx, document.getElementById('app'));
+  });
 });
--- Udemy/fullstack_web_apps_with_meteor_and_react/meteor-projects/score-keep/server/main.js
+++ Udemy/fullstack_web_apps_with_meteor_and_react/meteor-projects/score-keep/server/main.js
@@ -2,9 +2,27 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(function () {
-  Players.insert({
-    name: 'Sungbin',
-    score: 33
-  });
-  console.log(Players.find().fetch());
+
+  // function square (x) {
+  //   return x * x;
+  // };
+
+  // let square = (x) => {
+  //   return x * x;
+  // };
+  // console.log(square(10));
+
+  // let user = {
+  //   name: 'Kaden',
+  //   sayHi () {
+  //     setTimeout(() => {
+  //       console.log(this.name);
+  //     }, 1000);
+  //   }
+  // };
+  // user.sayHi(1, 2);
+
+  let numbers = [9, 123, 12, 33];
+  let newNumbers = numbers.map((number) => number * number);
+  console.log(newNumbers);
 });
