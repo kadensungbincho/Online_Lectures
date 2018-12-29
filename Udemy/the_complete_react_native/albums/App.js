f266b176eedbf7a54cda3cbffb899cc349bb4268
kadensungbincho <kadensungbincho@gmail.com>
2018-12-29 16:18:07 +0900

    Add new course on react-native

---
+++ Udemy/the_complete_react_native/albums/App.js
@@ -0,0 +1,29 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {
+  Platform, Text, View,
+} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n'
+       + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n'
+    + 'Shake or press menu button for dev menu',
+});
+
+const App = () => (
+  <View>
+    <Text>Some Text</Text>
+    <Text>{instructions}</Text>
+  </View>
+);
+
+export default App;
