2faba8051ad2d885daa8ff6b891f54b711635ac1
kadensungbincho <kadensungbincho@gmail.com>
2019-01-01 16:16:43 +0900

    update

--- Udemy/the_complete_react_native/auth/src/App.js
+++ Udemy/the_complete_react_native/auth/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCLQ20Mlfxr7szoPRPrw_0wEXJ7i9yfBiM',
@@ -14,13 +16,36 @@ class App extends Component {
       storageBucket: 'authentication-b6dbc.appspot.com',
       messagingSenderId: '329635077796'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     );
   }
--- Udemy/the_complete_react_native/auth/src/components/LoginForm.js
+++ Udemy/the_complete_react_native/auth/src/components/LoginForm.js
@@ -1,28 +1,85 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { text: '' };
+  state = { email: '', password: '', error: '', loading: false };
+
+  onButtonPress() {
+    const { email, password } = this.state;
+
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+         .then(this.onLoginSuccess.bind(this))
+         .catch(this.onLoginFail.bind(this));
+    });
+  }
+
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed', loading: false })
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: '',
+    });
+  }
+
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput
-            value={this.state.text}
-            onChangeText={text => this.setState({ text })}
-            style={{ height: 20, width: 100 }}
+          <Input
+            placeholder="user@gmail.com"
+            label="Email"
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
-        <CardSection />
         <CardSection>
-          <Button>
-            Log in
-          </Button>
+          <Input
+            secureTextEntry={true}
+            placeholder="password"
+            label="Password"
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+          />
+        </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+        <CardSection>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
+  }
+};
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
   }
 };
 
