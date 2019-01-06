b96ab48f961d04b4042b4ec381903f7956043970
kadensungbincho <kadensungbincho@gmail.com>
2019-01-06 22:20:51 +0900

    update

--- Udemy/the_complete_react_native/manager/src/components/LoginForm.js
+++ Udemy/the_complete_react_native/manager/src/components/LoginForm.js
@@ -1,17 +1,19 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { Component } from 'react';
+import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 
 import {
-  Card, CardSection, Input, Button,
+  Card, CardSection, Input, Button, Spinner,
 } from './common';
-import { emailChanged, passwordChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
   constructor(props) {
     super(props);
     this.onEmailChange = this.onEmailChange.bind(this);
     this.onPasswordChange = this.onPasswordChange.bind(this);
+    this.onButtonPress = this.onButtonPress.bind(this);
   }
 
   onEmailChange(text) {
@@ -20,6 +22,35 @@ class LoginForm extends Component {
 
   onPasswordChange(text) {
     this.props.passwordChanged(text);
+  }
+
+  onButtonPress() {
+    const { email, password } = this.props;
+
+    this.props.loginUser({ email, password });
+  }
+
+  renderError() {
+    if (this.props.error) {
+      return (
+        <View style={{ backgroundColor: 'white' }}>
+          <Text style={styles.errorTextStyle}>
+            {this.props.error}
+          </Text>
+        </View>
+      );
+    }
+  }
+
+  renderButton() {
+    if (this.props.loading) {
+      return <Spinner size="large" />;
+    }
+    return (
+      <Button onPress={this.onButtonPress}>
+        Login
+      </Button>
+    );
   }
 
   render() {
@@ -42,21 +73,35 @@ class LoginForm extends Component {
             value={this.props.password}
           />
         </CardSection>
+
+        {this.renderError()}
+
         <CardSection>
-          <Button>
-            Login
-          </Button>
+          {this.renderButton()}
         </CardSection>
       </Card>
     );
   }
 }
 
-const mapStateToProps = (state) => {
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red',
+  },
+}
+
+const mapStateToProps = ({ auth }) => {
+  const {
+    email, password, error, loading
+  } = auth;
+
   return {
-    email: state.auth.email,
-    password: state.auth.password,
+    email, password, error, loading
   };
 };
 
-export default connect(mapStateToProps, { emailChanged, passwordChanged })(LoginForm);
+export default connect(mapStateToProps, {
+  emailChanged, passwordChanged, loginUser,
+})(LoginForm);
