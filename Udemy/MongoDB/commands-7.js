0b8cd417cad3608daecb4c52702546ec96971f7c
kadensungbincho <kadensungbincho@gmail.com>
2019-02-02 13:58:48 +0900

    update

--- Udemy/MongoDB/commands-7.js
+++ Udemy/MongoDB/commands-7.js
@@ -24,3 +24,65 @@ db.friends.aggregate([
     } 
   }
 ]).pretty()
+
+db.friends.aggregate([
+  { $unwind: "$examScores" },
+  { $project: { _id: 1, name: 1, age: 1, score: "$examScores.score" } },
+  { $sort: { examScores: -1 } },
+  { $group: { _id: "$_id", name: { $first: "$name" }, maxScore: { $max: "$score" } } },
+  { $sort: { maxScore: -1 } }
+]).pretty();
+
+db.persons.aggregate([
+  { 
+    $bucket: { 
+      groupBy: "$dob.age", 
+      boundaries: [0, 18, 30, 50, 80, 120], 
+      output: {
+        numPersons: { $sum: 1 },
+        averageAge: { $avg: "$dob.age" },
+        names: { $push: "$name.first" }
+      } 
+    } 
+  }
+]).pretty();
+
+db.persons.aggregate([
+  { 
+    $bucket: { 
+      groupBy: "$dob.age", 
+      boundaries: [18, 30, 40, 50, 60, 80], 
+      output: {
+        numPersons: { $sum: 1 },
+        averageAge: { $avg: "$dob.age" }
+      } 
+    } 
+  }
+]).pretty();
+
+db.persons.aggregate([
+  { 
+    $bucketAuto: {
+      groupBy: "$dob.age",
+      buckets: 5,
+      output: {
+        numPersons: { $sum: 1 },
+          averageAge: { $avg: "$dob.age" }
+      }
+    } 
+  }
+]).pretty();
+
+db.persons.aggregate([
+  { $project: { _id: 0, name: 1, birthdate: { $toDate: "$dob.date" } } },
+  { $sort: { birthdate: 1 } },
+  { $limit: 10 }
+]).pretty()
+
+db.persons.aggregate([
+  { $project: { _id: 0, name: { $concat: ["$name.first", " ", "$name.last"] }, birthdate: { $toDate: "$dob.date" } } },
+  { $sort: { birthdate: 1 } },
+  { $skip: 10 },
+  { $limit: 10 }
+]).pretty()
+
