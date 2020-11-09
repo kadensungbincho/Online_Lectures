7202f45cbb9d07c50ec3ccf83d0d79faf1039736
kadensungbincho <kadensungbincho@gmail.com>
2020-11-09 22:47:46 +0900

    More on spring

---
+++ fastcampus/java_byte_degree/adminApp/src/main/java/com/example/study/entity/User.java
@@ -0,0 +1,37 @@
+package com.example.study.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+
+}
---
+++ fastcampus/java_byte_degree/adminApp/src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -0,0 +1,35 @@
+package com.example.study.repository;
+
+import com.example.study.StudyApplicationTests;
+import com.example.study.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+
+public class UserRepositoryTest extends StudyApplicationTests {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Test
+    public void create() {
+        User user = new User();
+        user.setAccount("TestUser03");
+        user.setEmail("TestUser03@gmail.com");
+        user.setPhoneNumber("010-1111-1111");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setCreatedBy("admin");
+
+        User newUser = userRepository.save(user);
+        System.out.println(newUser);
+    }
+
+    public void read() {
+
+    }
+
+    public void update() {
+
+    }
+}
