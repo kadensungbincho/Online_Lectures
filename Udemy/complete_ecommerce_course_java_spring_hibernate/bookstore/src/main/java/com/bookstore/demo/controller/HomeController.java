6a8d472d9aa7af5082fc9aae83c96e94fc6410df
kadensungbincho <kadensungbincho@gmail.com>
2020-11-22 14:48:04 +0900

    More on ecommerce with JAva

--- Udemy/complete_ecommerce_course_java_spring_hibernate/bookstore/src/main/java/com/bookstore/demo/controller/HomeController.java
+++ Udemy/complete_ecommerce_course_java_spring_hibernate/bookstore/src/main/java/com/bookstore/demo/controller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/myAccount")
+    public String myAccount() {
+        return "myAccount";
+    }
 }
