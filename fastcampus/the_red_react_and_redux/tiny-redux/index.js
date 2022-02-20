21afbfe9c4c41a9041ffedaf51c6c980c6e743bb
user <user@AD01980712.local>
2022-02-20 22:04:45 +0900

    tiny-redux'

--- fastcampus/the_red_react_and_redux/tiny-redux/index.js
+++ fastcampus/the_red_react_and_redux/tiny-redux/index.js
@@ -1,10 +1,64 @@
 import { createStore } from './redux';
 
-function updater() {
+const COUNTER = 'count';
+const FETCHER = 'fetch';
 
+const middleware1 = (store) => (dispatch) => (action) => {
+    console.log('middlewaere1');
+
+    if (action.type === FETCHER) {
+        console.log(FETCHER);
+        setTimeout(() => {
+            dispatch({
+                type: 'fetch-response',
+                payload: [
+                    1, 2, 3
+                ]
+            });
+        }, 2000);
+    } else {
+        dispatch(action);
+    }
 }
 
+const middleware2 = (store) => (dispatch) => (action) => {
+    console.log('middlewaere2');
+    dispatch(action);
+}
 
-const store = createStore(updater);
+function reducer(state, action) {
+    if (action.type === COUNTER) {
+        return { ...state, counter: action.playload.counter };
+    }
 
-store.doUpdate();
+    if (action.type === 'fetch-response') {
+        return { ...state, response: action.payload };
+    }
+    
+    return state;
+}
+
+function listener() {
+    console.log(store.getState());
+}
+
+function actionCreator(type, payload) {
+    return {
+        type,
+        payload,
+    }
+}
+
+const store = createStore(reducer, [middleware1, middleware2]);
+
+store.subscribe(listener);
+
+// store.dispatch(actionCreator(COUNTER, { counter: 1 }));
+
+function counter(data) {
+    store.dispatch(actionCreator(COUNTER, data));
+}
+
+counter({ counter: 1});
+
+store.dispatch(actionCreator(FEFETCHERTCH))
--- fastcampus/the_red_react_and_redux/tiny-redux/redux.js
+++ fastcampus/the_red_react_and_redux/tiny-redux/redux.js
@@ -1,12 +1,36 @@
-export function createStore(updater) {
+export function createStore(reducer, middleware = []) {
     let state;
+    const handler = [];
 
 
-    function doUpdate() {
-        updater();
+    function dispatch(action) {
+        state = reducer(state, action);
+        handler.forEach(listener => {
+            listener();
+        })
     }
+
+    function getState() {
+        return state;
+    }
+
+    function subscribe(listener) {
+        handler.push(listener);
+    }
+
+    const store = {
+        getState,
+        subscribe,
+    }
+    middleware = Array.from(middleware).reverse();
+    let lastDispatch = dispatch;
+
+    middleware.forEach(m => {
+        lastDispatch = m(store)(lastDispatch);
+    })
 
     return {
-        doUpdate,
-    }
+        ...store,
+        dispatch: lastDispatch
+    };
 }
